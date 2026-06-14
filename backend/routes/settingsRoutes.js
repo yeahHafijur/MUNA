3f59f67eef222aff46afd109bc6016e1997fec21
yeahHafijur <yhz01012004@gmail.com>
2026-06-14 12:35:31 +0530

    fix: Update authorization middleware import in settings routes

--- backend/routes/settingsRoutes.js
+++ backend/routes/settingsRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getNavbarMessage, updateNavbarMessage } = require('../controllers/settingsController');
-const { protect, admin } = require('../middleware/authMiddleware');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Public route to get navbar message
 router.get('/navbar-message', getNavbarMessage);
 
 // Protected route (SuperAdmin) to update navbar message
-router.put('/navbar-message', protect, admin, updateNavbarMessage);
+router.put('/navbar-message', protect, authorize('super_admin'), updateNavbarMessage);
 
 module.exports = router;

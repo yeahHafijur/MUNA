a552a5cae75d6c35cbacfbe65bc209826ab1809d
yeahHafijur <yhz01012004@gmail.com>
2026-06-16 20:45:36 +0530

    Merge static and dynamic urls into single backend sitemap route

--- backend/index.js
+++ backend/index.js
@@ -86,10 +86,41 @@ app.get('/api/sitemap.xml', async (req, res) => {
         const Shop = require('./models/Shop');
         const shops = await Shop.find({ isActive: true });
         
-        let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+        let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://www.munastore.in/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://www.munastore.in/privacy-policy</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    <url>
+        <loc>https://www.munastore.in/terms-and-conditions</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    <url>
+        <loc>https://www.munastore.in/about-us</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <url>
+        <loc>https://www.munastore.in/contact</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>`;
 
         shops.forEach(shop => {
-            xml += `\n    <url>\n        <loc>https://www.munastore.in/shop/${shop._id}</loc>\n        <changefreq>weekly</changefreq>\n        <priority>0.8</priority>\n    </url>`;
+            xml += `
+    <url>
+        <loc>https://www.munastore.in/shop/${shop._id}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>`;
         });
 
         xml += `\n</urlset>`;

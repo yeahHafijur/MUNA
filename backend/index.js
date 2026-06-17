6a75b5484fa28d73939eb23953d26cfab0c9a485
yeahHafijur <yhz01012004@gmail.com>
2026-06-17 07:41:16 +0530

    Fix CORS by explicitly allowing www and non-www domains

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4173',
     'http://127.0.0.1:5173',
+    'https://munastore.in',
+    'https://www.munastore.in',
     process.env.FRONTEND_URL   // Set this in .env for production
 ].filter(Boolean);
 
